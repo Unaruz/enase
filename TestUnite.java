9a51e4f1a1973bfed4167d62a1b359f785259764
Unaruz <Unaruz@gmail.com>
2018-11-28 22:14:08 +0100

    Création d'un programme de test pour la classe Unite

---
+++ TestUnite.java
@@ -0,0 +1,7 @@
+class TestUnite {
+    public static void main(String[] args) {
+        Unite u1 = new Unite("Amazon", 123456789, 4324);
+        Unite u2 = new Unite("Free", 198765432, 7865);
+        System.out.println(u1.nom + " / " + u1.ca);
+    }
+}
