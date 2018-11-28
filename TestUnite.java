46b82991b8a595f2d127850c96d4fb213cd9a4a5
unaruz <unaruz@gmail.com>
2018-11-28 22:34:30 +0100

    Affichage d'une chaine de caractère

--- TestUnite.java
+++ TestUnite.java
@@ -1,7 +1,12 @@
+/*
+ * Entete du la classe test
+ */
 class TestUnite {
     public static void main(String[] args) {
+    	
         Unite u1 = new Unite("Amazon", 123456789, 4324);
         Unite u2 = new Unite("Free", 198765432, 7865);
+        System.out.println("\nInformation sur Amazon\n");
         System.out.println(u1.nom + " / " + u1.ca);
     }
 }
