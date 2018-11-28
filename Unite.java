f9e36f72bf1f3125693ea98666e800a22c94dd5b
Unaruz <Unaruz@gmail.com>
2018-11-28 22:13:13 +0100

    Création de la classe Unite

---
+++ Unite.java
@@ -0,0 +1,15 @@
+class Unite {
+    String nom;
+    double siren;
+    double ca;
+
+    public Unite(String nom, double siren, double ca) {
+        this.nom = nom;
+        this.siren = siren;
+        this.ca = ca;
+    }
+    public void modifierCa(double nouveauCA) {
+        this.ca = nouveauCA;
+    }
+
+}
